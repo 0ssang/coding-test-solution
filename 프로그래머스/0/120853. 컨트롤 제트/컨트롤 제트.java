ce538068ab90db1f81f840b0fd23621f3ea72f4d
조영상 <51320595+0ssang@users.noreply.github.com>
2024-03-28 19:06:44 +0900

    [level 0] Title: 컨트롤 제트, Time: 0.29 ms, Memory: 74.2 MB -BaekjoonHub

--- 프로그래머스/0/120853. 컨트롤 제트/컨트롤 제트.java
+++ 프로그래머스/0/120853. 컨트롤 제트/컨트롤 제트.java
@@ -1,18 +1,30 @@
+import java.util.*;
 class Solution {
     public int solution(String s) {
-        String[] str = s.split(" ");
-        int sum = 0;
-        int prev = 0;
+        //String[] str = s.split(" ");
+        //int sum = 0;
+        //int prev = 0;
         
-        for(int i=0; i<str.length; i++){
-            if(!str[i].equals("Z")){
-                prev = Integer.parseInt(str[i]);
-                sum += prev;
+        //for(int i=0; i<str.length; i++){
+            //if(!str[i].equals("Z")){
+                //prev = Integer.parseInt(str[i]);
+                //sum += prev;
+            //}else{
+                //sum -= prev;
+            //}
+        //}
+        int sum = 0;
+        Stack<Integer> stack = new Stack<>();
+        for(String str : s.split(" ")){
+            if(!str.equals("Z")){
+                stack.push(Integer.parseInt(str));
             }else{
-                sum -= prev;
+                stack.pop();
             }
         }
-        
+        for(int i : stack){
+            sum += i;
+        }
         return sum;
     }
 }
