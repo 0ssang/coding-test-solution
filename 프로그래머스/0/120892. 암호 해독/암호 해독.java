4f31d6cdc1429a80142961c46ec992e813bb7649
조영상 <51320595+0ssang@users.noreply.github.com>
2024-03-28 21:05:16 +0900

    [level 0] Title: 암호 해독, Time: 1.86 ms, Memory: 79.9 MB -BaekjoonHub

--- 프로그래머스/0/120892. 암호 해독/암호 해독.java
+++ 프로그래머스/0/120892. 암호 해독/암호 해독.java
@@ -1,12 +1,17 @@
 class Solution {
     public String solution(String s, int c) {
-        char[] chars = s.toCharArray();
-        StringBuilder sb = new StringBuilder();
+//         char[] chars = s.toCharArray();
+//         StringBuilder sb = new StringBuilder();
         
-        for(int i=0; i<chars.length; i++)
-            if((i+1) % c == 0)
-                sb.append(chars[i]);
+//         for(int i=0; i<chars.length; i++)
+//             if((i+1) % c == 0)
+//                 sb.append(chars[i]);
         
-        return sb.toString();
+//         return sb.toString();
+        String answer = "";
+        for(int i=c-1; i<s.length(); i+=c){
+            answer += s.substring(i, i+1);
+        }
+        return answer;
     }
 }
