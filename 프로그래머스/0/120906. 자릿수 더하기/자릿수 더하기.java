57b8e886c9ead40cd9339d049de5c0811f7ae66d
조영상 <51320595+0ssang@users.noreply.github.com>
2024-04-10 00:00:34 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.02 ms, Memory: 81.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int n) {
+        int sum = 0;
+        while(n/10 > 0){
+            sum += n%10;
+            n /= 10;
+        }
+        sum += n;
+        
+        return sum;
+    }
+}
