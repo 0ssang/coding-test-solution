4a88748ee404818bd3ee9f168f283cc5be480c24
조영상 <51320595+0ssang@users.noreply.github.com>
2024-04-11 00:52:21 +0900

    [level 0] Title: 문자열 정렬하기 (2), Time: 0.40 ms, Memory: 75 MB -BaekjoonHub

--- 프로그래머스/0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.java
+++ 프로그래머스/0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.java
@@ -1,9 +1,12 @@
-import java.util.stream.*;
+import java.util.*;
 class Solution {
     public String solution(String str) {
-        return Stream.of(str.split(""))
-            .map(String::toLowerCase)
-            .sorted()
-            .collect(Collectors.joining());
+        // return Stream.of(str.split(""))
+        //     .map(String::toLowerCase)
+        //     .sorted()
+        //     .collect(Collectors.joining());
+        char[] chars = str.toLowerCase().toCharArray();
+        Arrays.sort(chars);
+        return new String(chars);
     }
 }
