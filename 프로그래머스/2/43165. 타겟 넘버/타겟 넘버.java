65a7fcb3cb9da7c8562e1daa9808d1185435c926
조영상 <51320595+0ssang@users.noreply.github.com>
2024-04-16 01:56:03 +0900

    [level 2] Title: 타겟 넘버, Time: 0.33 ms, Memory: 75.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -0,0 +1,28 @@
+import java.util.*;
+class Solution {
+    int[] numbers;
+    int target;
+    int answer;
+    
+    void dfs(int index, int sum){
+        //1. 탈출 조건
+        if(index == numbers.length){
+            if(sum == target) answer++;
+            return;
+        }
+        
+        //2. 수행 동장
+        dfs(index + 1, sum + numbers[index]);
+        dfs(index + 1, sum - numbers[index]);
+    }
+    
+    public int solution(int[] numbers, int target) {
+        this.numbers = numbers;
+        this.target = target;
+        answer = 0;
+        
+        dfs(0, 0);
+        
+        return answer;
+    }
+}
