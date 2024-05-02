3dc45d38a52f0b5d7b28b7ce61e0feba340ecd3c
조영상 <51320595+0ssang@users.noreply.github.com>
2024-05-02 21:16:57 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 89.47 ms, Memory: 94.6 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,22 +1,21 @@
 import java.util.*;
 class Solution {
-    public String solution(String[] participant, String[] completion) {
-        String answer = "";
-        HashMap<String, Integer> map = new HashMap<>();
-        for(String name : participant){
-            map.put(name, map.getOrDefault(name, 0) +1);
+    public String solution(String[] parti, String[] comp) {
+        Map<String, Integer> map = new HashMap<>();
+        // 해시맵에 참가자 명단 업데이트
+        for(String name : parti){
+            map.put(name, map.getOrDefault(name, 0) + 1);
         }
-        for(String name : completion){
-            map.put(name, map.get(name) -1);
+        // 완주자 명단에 들어 있으면 값 -1
+        for(String name : comp){
+            map.put(name, map.get(name) - 1);
         }
-        Iterator<Map.Entry<String, Integer>> it = map.entrySet().iterator();
-        while(it.hasNext()){
-            Map.Entry<String, Integer> entry = it.next();
-            if(entry.getValue() != 0){
-                answer = entry.getKey();
-                break;
-            }
+        // 해시맵 검색하여 0보다 큰 값이 있으면 return
+        String res = "";
+        Set<String> keys = map.keySet();
+        for(String key : keys){
+            if(map.get(key) > 0)res = key;
         }
-        return answer;
+        return res;
     }
 }
