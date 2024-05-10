ea96fc1b76cf9c90968e402202817f4d8f681ef2
조영상 <51320595+0ssang@users.noreply.github.com>
2024-05-10 18:29:21 +0900

    [level 2] Title: 의상, Time: 0.07 ms, Memory: 70.2 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -2,15 +2,16 @@ import java.util.*;
 
 class Solution {
     public int solution(String[][] clothes) {
-        HashMap<String, Integer> hm = new HashMap<>();
-        for(String[] clothe : clothes){
-            hm.put(clothe[1], hm.getOrDefault(clothe[1], 0) + 1);
+        int res = 1;
+        Map<String, Integer> map = new HashMap<>();
+        for(String[] cloth : clothes) {
+            map.put(cloth[1], map.getOrDefault(cloth[1], 0) + 1);
         }
-        Iterator<Integer> it = hm.values().iterator();
-        int answer = 1;
-        while(it.hasNext()){
-            answer *= it.next().intValue()+1;
+        Set<String> keys = map.keySet();
+        for(String key : keys){
+            res *= map.get(key)+1;
         }
-        return answer - 1;
+        
+        return res - 1;
     }
 }
