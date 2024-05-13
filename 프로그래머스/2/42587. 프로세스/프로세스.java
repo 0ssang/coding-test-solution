6365ff4ac5311e502eada2d5e15b153578efd337
조영상 <51320595+0ssang@users.noreply.github.com>
2024-05-13 23:46:38 +0900

    [level 2] Title: 프로세스, Time: 0.48 ms, Memory: 76.8 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -1,19 +1,18 @@
 import java.util.*;
 class Solution {
     public int solution(int[] p, int l) {
-        int rank = 1;
+        
         PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
-
-        for (int j : p) {
+        int rank = 1;
+        
+        for (int j : p)
             pq.add(j);
-        }
 
         while(!pq.isEmpty()){
             for(int i=0; i<p.length; i++){
                 if(p[i] == pq.peek()){
-                    if(i == l){
+                    if(i == l)
                         return rank;
-                    }
                     pq.poll();
                     rank++;
                 }
