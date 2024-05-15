4662f70c8b7f54e5d68cd0a6ce99f16b511cf253
조영상 <51320595+0ssang@users.noreply.github.com>
2024-05-15 20:11:20 +0900

    [level 2] Title: 주식가격, Time: 21.72 ms, Memory: 64.2 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -1,18 +1,36 @@
+import java.util.*;
 class Solution {
     public int[] solution(int[] prices) {
-        for(int i = 0; i < prices.length - 1; i++){
-            int cnt = 0;
-            for(int j = i+1; j < prices.length; j++){
-                if(prices[i] <= prices[j]){
-                    cnt++;
-                }else {
-                    cnt++;
-                    break;
-                }
+        // for(int i = 0; i < prices.length - 1; i++){
+        //     int cnt = 0;
+        //     for(int j = i+1; j < prices.length; j++){
+        //         if(prices[i] <= prices[j]){
+        //             cnt++;
+        //         }else {
+        //             cnt++;
+        //             break;
+        //         }
+        //     }
+        //     prices[i] = cnt;
+        // }
+        // prices[prices.length-1] = 0;
+        // return prices;
+        Stack<Integer> beginIdxs = new Stack<>();
+        int i=0;
+        int[] terms = new int[prices.length];
+
+        beginIdxs.push(i);
+        for (i=1; i<prices.length; i++) {
+            while (!beginIdxs.empty() && prices[i] < prices[beginIdxs.peek()]) {
+                int beginIdx = beginIdxs.pop();
+                terms[beginIdx] = i - beginIdx;
             }
-            prices[i] = cnt;
+            beginIdxs.push(i);
         }
-        prices[prices.length-1] = 0;
-        return prices;
+        while (!beginIdxs.empty()) {
+            int beginIdx = beginIdxs.pop();
+            terms[beginIdx] = i - beginIdx - 1;
+        }
+        return terms;
     }
 }
