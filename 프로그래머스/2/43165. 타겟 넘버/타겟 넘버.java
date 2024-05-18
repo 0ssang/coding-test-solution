f302c10009cc3bf3274d7c11c12c33cffccc1c4a
조영상 <51320595+0ssang@users.noreply.github.com>
2024-05-18 23:44:47 +0900

    [level 2] Title: 타겟 넘버, Time: 0.32 ms, Memory: 75.4 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,28 +1,24 @@
-import java.util.*;
-class Solution {
-    int[] numbers;
-    int target;
-    int answer;
-    
-    void dfs(int index, int sum){
-        //1. 탈출 조건
-        if(index == numbers.length){
-            if(sum == target) answer++;
-            return;
-        }
-        
-        //2. 수행 동장
-        dfs(index + 1, sum + numbers[index]);
-        dfs(index + 1, sum - numbers[index]);
-    }
-    
-    public int solution(int[] numbers, int target) {
-        this.numbers = numbers;
-        this.target = target;
-        answer = 0;
+class Solution {    
+    static int[] numbers;
+    static int target;
+    static int answer = 0;
+
+    public int solution(int[] nums, int t) {
+        numbers = nums;
+        target = t;
         
         dfs(0, 0);
         
         return answer;
+    }
+    
+    public static void dfs(int sum, int depth){
+        if(depth == numbers.length){
+            if(target == sum) answer++;
+            return;
+        }
+
+        dfs(sum + numbers[depth], depth + 1);
+        dfs(sum - numbers[depth], depth + 1);
     }
 }
