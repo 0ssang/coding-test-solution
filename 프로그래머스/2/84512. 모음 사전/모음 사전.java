7ee36e2a18f9c5d158962ff475a960a6ebc98dc1
조영상 <51320595+0ssang@users.noreply.github.com>
2024-05-18 20:36:18 +0900

    [level 2] Title: 모음 사전, Time: 10.59 ms, Memory: 79.9 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -1,3 +1,32 @@
+// 성능비교하기
+// import java.util.*;
+
+// class Solution {
+//     static int cnt;
+//     static final char[] dic = {'A','E','I','O','U'};
+    
+//     public int solution(String word) {
+//         Map<String, Integer> map = new HashMap<>();
+        
+//         dfs(map, "", 0);
+        
+//         return map.get(word);
+//     }
+    
+//     public static void dfs(Map<String, Integer> map,String s, int depth){
+//         // 탈출조건
+//         if(depth >= dic.length) return;
+        
+//         for(int i = 0; i < dic.length; i++){
+//             if(!map.containsKey(s+dic[i])){
+//                 String now = s + dic[i];
+//                 map.put(now, ++cnt);
+//                 dfs(map, now, depth + 1);
+//             }
+//         }
+//     }
+// }
+
 import java.util.*;
 
 class Solution {
@@ -5,21 +34,25 @@ class Solution {
     static final char[] dic = {'A','E','I','O','U'};
     
     public int solution(String word) {
-        Map<String, Integer> map = new HashMap<>();
+        List<String> list = new ArrayList<>();
+        list.add("init");
+        dfs(list, word, "", 0);
         
-        dfs(map, "", 0);
-        
-        return map.get(word);
+        return cnt;
     }
     
-    public static void dfs(Map<String, Integer> map, String s, int depth){
+    public static void dfs(List<String> list, String word, String s, int depth){
         // 탈출조건
         if(depth >= dic.length) return;
         
         for(int i = 0; i < dic.length; i++){
-            String now = s + dic[i];
-            map.put(now, ++cnt);
-            dfs(map, now, depth + 1);
+            if(!word.equals(list.get(cnt))){
+                String now = s + dic[i];
+                list.add(now);
+                ++cnt;
+                dfs(list, word, now, depth + 1);
+            }
+            
         }
     }
 }
