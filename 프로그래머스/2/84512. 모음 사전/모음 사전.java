3b58dc9756da7e75e8ac70a3ddfaf5ca6e441f9e
조영상 <51320595+0ssang@users.noreply.github.com>
2024-05-28 21:46:03 +0900

    [level 2] Title: 모음 사전, Time: 29.28 ms, Memory: 75.9 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -32,16 +32,15 @@ import java.util.*;
 class Solution {
     static int cnt;
     static final char[] dic = {'A','E','I','O','U'};
-    
+    static List<String> list = new ArrayList<>();
     public int solution(String word) {
-        List<String> list = new ArrayList<>();
         list.add("init");
-        dfs(list, word, "", 0);
+        dfs(word, "", 0);
         
         return cnt;
     }
     
-    public static void dfs(List<String> list, String word, String s, int depth){
+    public static void dfs(String word, String s, int depth){
         // 탈출조건
         if(depth >= dic.length) return;
         
@@ -50,7 +49,7 @@ class Solution {
                 String now = s + dic[i];
                 list.add(now);
                 ++cnt;
-                dfs(list, word, now, depth + 1);
+                dfs(word, now, depth + 1);
             }
             
         }
