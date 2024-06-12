11fef09532f8da34626656d75df836ac41577d27
조영상 <51320595+0ssang@users.noreply.github.com>
2024-06-12 23:28:05 +0900

    [Silver V] Title: 요세푸스 문제 0, Time: 128 ms, Memory: 14192 KB -BaekjoonHub

--- 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.java
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.java
@@ -1,36 +1,43 @@
 import java.util.*;
 import java.io.*;
 public class Main {
- 
-	public static void main(String[] args) throws IOException {
-		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		Queue<Integer> q = new LinkedList<>();
-		
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		
-		int N = Integer.parseInt(st.nextToken());
-		int K = Integer.parseInt(st.nextToken());
-		
-		
-		for(int i = 1; i <= N; i++) {
-			q.add(i);
-		}
-		
-		StringBuilder sb = new StringBuilder();
-		sb.append('<');
-	
-		while(q.size() > 1) {
-			
-			for(int i = 0; i < K - 1; i++) {
-				q.offer(q.poll());
-			}
-			
-			sb.append(q.poll()).append(", ");
-		}
- 
-		sb.append(q.poll()).append('>');
-		System.out.println(sb);
-	}
- 
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        // Queue를 이용한 풀이
+//        Queue<Integer> q = new LinkedList<>();
+//        for(int i=1; i<=N; i++){
+//            q.offer(i);
+//        }
+//
+//        sb.append("<");
+//
+//        while(q.size() > 1){
+//            for(int i=0; i<K-1; i++){
+//                q.offer(q.poll());
+//            }
+//            sb.append(q.poll()).append(", ");
+//        }
+//
+//        sb.append(q.poll()).append(">");
+//        System.out.println(sb);
+        // List를 이용한 풀이
+        LinkedList<Integer> list = new LinkedList<>();
+        for(int i=1; i<=N; i++){
+            list.add(i);
+        }
+        int idx = 0;
+        sb.append('<');
+        while(N > 1){
+            idx = (idx + (K-1)) % N;
+            sb.append(list.remove(idx)).append(", ");
+            N--;
+        }
+        sb.append(list.remove()).append('>');
+        System.out.println(sb);
+    }
 }
