8b8c537c1326aefd81e31b06b6f52c8b18c88264
Youngsang Cho <51320595+0ssang@users.noreply.github.com>
2024-08-06 23:47:42 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 136 ms, Memory: 9436 KB -BaekjoonHub

---
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.js
@@ -0,0 +1,14 @@
+const readline = require("readline");
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+let input = [];
+rl.on("line", (line) => {
+    input = line.split(" ");
+    if(+input[0] > +input[1]) console.log(">");
+    else if(+input[0] < +input[1]) console.log("<");
+    else console.log("==");
+    process.exit();
+});
