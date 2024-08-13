b6025d9bcfeecf4d431543ead316f30248a57944
Youngsang Cho <51320595+0ssang@users.noreply.github.com>
2024-08-14 02:22:24 +0900

    [Bronze III] Title: 최댓값, Time: 116 ms, Memory: 9324 KB -BaekjoonHub

--- 백준/Bronze/2562. 최댓값/최댓값.js
+++ 백준/Bronze/2562. 최댓값/최댓값.js
@@ -1,4 +1,3 @@
 const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n').map(Number);
 const max = Math.max(...input);
-console.log(max);
-console.log(input.indexOf(max)+1);
+console.log(`${max}\n${input.indexOf(max)+1}`);
