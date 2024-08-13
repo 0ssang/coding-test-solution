4da52f0f19c1f27c1881616cf540c53787f4239c
Youngsang Cho <51320595+0ssang@users.noreply.github.com>
2024-08-13 23:55:15 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 136 ms, Memory: 9336 KB -BaekjoonHub

---
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.js
@@ -0,0 +1,6 @@
+const input = require('fs').readFileSync('/dev/stdin').toString().trim();
+const arr = [];
+for(let i=97; i<=122; i++){
+    arr.push(input.indexOf(String.fromCharCode(i)));
+}
+console.log(arr.join(" "));
