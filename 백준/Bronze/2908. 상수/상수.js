e43d91e5e2890eea0bea4801c34fa1fa57e788a0
Youngsang Cho <51320595+0ssang@users.noreply.github.com>
2024-08-14 00:03:15 +0900

    [Bronze II] Title: 상수, Time: 124 ms, Memory: 9324 KB -BaekjoonHub

---
+++ 백준/Bronze/2908. 상수/상수.js
@@ -0,0 +1,4 @@
+const input = require('fs').readFileSync('/dev/stdin').toString().trim().split(" ");
+const first = input[0].split('').reverse().join('');
+const second = input[1].split('').reverse().join('');
+console.log(+first > +second ? first : second);
