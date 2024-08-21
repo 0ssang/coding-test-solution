71b5ed70d9517b20807e8514f5bc94b0eeeb2ae0
Youngsang Cho <51320595+0ssang@users.noreply.github.com>
2024-08-22 00:08:07 +0900

    [Silver V] Title: 숫자 카드, Time: 620 ms, Memory: 148312 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.js
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.js
@@ -3,14 +3,14 @@ const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('
 const n = input[1].split(" ").map(Number);
 const m = input[3].split(" ").map(Number);
 
-const map = new Map();
+const set = new Set();
 const result = [];
-n.forEach((val, idx) => {
-    map.set(val, idx);
+n.forEach((val) => {
+    set.add(val);
 });
 
 m.forEach(val => {
-    map.has(val) ? result.push(1) : result.push(0);
+    set.has(val) ? result.push(1) : result.push(0);
 });
 
 console.log(result.join(" "));
