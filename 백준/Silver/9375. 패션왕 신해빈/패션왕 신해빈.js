254c100ca05bdc03a23fdb68de7db026de92533a
Youngsang Cho <51320595+0ssang@users.noreply.github.com>
2024-08-28 02:04:13 +0900

    [Silver III] Title: 패션왕 신해빈, Time: 88 ms, Memory: 9872 KB -BaekjoonHub

--- 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.js
+++ 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.js
@@ -1,22 +1,23 @@
 const input = require('fs').readFileSync('/dev/stdin').toString().trim().split("\n");
 let testcase = +input.shift();
 const answer = [];
+const map = new Map();
+let combination = 1;
 
 while(testcase > 0){
-    const map = new Map();
     let n = +input.shift();
 
     for(let i=0; i<n; i++){
         let [val, key] = input.shift().split(/\s/);
         map.has(key) ? map.get(key).push(val) : map.set(key, [val]);
     }
-
-    let combination = 1;
     map.forEach((val) => {
         combination *= val.length + 1;
     });
 
     answer.push(--combination);
+    map.clear();
+    combination = 1;
     testcase--;
 }
 console.log(answer.join("\n"));
