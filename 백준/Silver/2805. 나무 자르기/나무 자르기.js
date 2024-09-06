d81b364662adb16e89e34449d43be97670e50fd1
Youngsang Cho <51320595+0ssang@users.noreply.github.com>
2024-09-06 19:45:24 +0900

    [Silver II] Title: 나무 자르기, Time: 960 ms, Memory: 136932 KB -BaekjoonHub

---
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.js
@@ -0,0 +1,23 @@
+const path = process.platform === 'linux' ? '/dev/stdin' : 'stdin.txt';
+const input = require('fs').readFileSync(path).toString().trim().split(/\r?\n/);
+let [num, target] = input[0].split(" ").map(Number)
+let tree = input[1].split(" ").map(Number).sort((a, b) => a - b)
+
+let lo = 1
+let hi = tree[num-1]
+let answer = 0
+while(lo <= hi){
+    let mid = Math.floor((lo + hi) / 2)
+    let sum = 0
+    for(const x of tree){
+        sum += Math.max(0, x - mid)
+    }
+
+    if(sum >= target){
+        answer = mid
+        lo = mid + 1
+    }else if(sum < target){
+        hi = mid - 1
+    }
+}
+console.log(answer)
