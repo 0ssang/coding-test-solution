56f807fcf76bbd4eb8a819a0e8609ec73aab4e2a
Youngsang Cho <51320595+0ssang@users.noreply.github.com>
2024-11-12 23:43:31 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 612 ms, Memory: 125260 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -1,20 +1,24 @@
 import java.util.*;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-
-        ArrayList<Integer> list =  new ArrayList<>();
-        for(int i = 0; i < N; i++){
-            list.add(sc.nextInt());
+        
+        int tc = Integer.parseInt(br.readLine());
+        boolean[] arr = new boolean[2000001];
+        
+        for (int i = 0; i < tc; i++) {
+            arr[Integer.parseInt(br.readLine()) + 1000000] = true;
         }
-
-        Collections.sort(list);
-        for(int v : list){
-            sb.append(v).append('\n');
-        }
+        
+        for(int i = 0; i < arr.length; i++) {
+			if(arr[i]) {
+				sb.append((i - 1000000)).append('\n');
+			}
+		}
+        
         System.out.println(sb);
     }
 }
