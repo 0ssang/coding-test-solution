9a53bba3e3be98b74c709303ca10a9ef9e65b020
Youngsang Cho <51320595+0ssang@users.noreply.github.com>
2024-11-14 19:40:05 +0900

    [Bronze I] Title: 팰린드롬수, Time: 88 ms, Memory: 12812 KB -BaekjoonHub

--- 백준/Bronze/1259. 팰린드롬수/팰린드롬수.java
+++ 백준/Bronze/1259. 팰린드롬수/팰린드롬수.java
@@ -1,16 +1,35 @@
-import java.io.*;
-public class Main {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        while(true){
-            String str = br.readLine();
-            if(str.equals("0")) break;
-            StringBuilder s = new StringBuilder(str);
-            if(str.equals(s.reverse().toString())) sb.append("yes").append('\n');
-            else sb.append("no").append('\n');
-        }
-        System.out.println(sb);
+import java.util.*;
 
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+        
+        while(true){
+            String line = sc.nextLine();
+            if(line.equals("0")) break;
+            
+            boolean isPall = true;
+            int start = 0;
+            int end = line.length() - 1;
+            if(line.length() > 1) {
+                while(start < end){
+                    if(line.charAt(start) != line.charAt(end)){
+                        isPall = false;
+                        break;
+                    }
+                    start++;
+                    end--;
+                }
+            }
+            if(isPall){
+                sb.append("yes");
+            } else {
+                sb.append("no");
+            }
+            sb.append('\n');
+        }
+        sc.close();    
+        System.out.println(sb);
     }
 }
