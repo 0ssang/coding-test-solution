8614741573b7c48715674459db281d47d1fb296b
Youngsang Cho <51320595+0ssang@users.noreply.github.com>
2024-11-14 20:01:12 +0900

    [Bronze I] Title: 팰린드롬수, Time: 92 ms, Memory: 9436 KB -BaekjoonHub

--- 백준/Bronze/1259. 팰린드롬수/팰린드롬수.js
+++ 백준/Bronze/1259. 팰린드롬수/팰린드롬수.js
@@ -1,7 +1,18 @@
 const input = require('fs').readFileSync('/dev/stdin').toString().trim().split("\n");
 input.pop();
 input.forEach(str => {
-    const reversed = [...str].reverse().join('');
-    if(reversed === str) console.log("yes");
-    else console.log("no");
+    let isPalindrome = true;
+    let start = 0;
+    let end = str.length - 1;
+
+    while (start < end) {
+        if (str[start] !== str[end]) {
+            isPalindrome = false;
+            break;
+        }
+        start++;
+        end--;
+    }
+
+    console.log(isPalindrome ? "yes" : "no");
 });
