0c9d3a67046aabddeba05f15984db7f081ae6e06
Youngsang Cho <51320595+0ssang@users.noreply.github.com>
2024-12-05 23:49:38 +0900

    [Silver IV] Title: 수 찾기, Time: 492 ms, Memory: 52556 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,39 +1,21 @@
 import java.util.*;
 import java.io.*;
-public class Main {
-    static boolean[][] board;
-    static int min = 64;
-    public static void main(String[] args) throws Exception {
+
+public class Main{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
+        Set<Integer> set = new HashSet<>();
         int N = Integer.parseInt(br.readLine());
-        int[] arr = new int[N];
         StringTokenizer st = new StringTokenizer(br.readLine());
         for(int i=0; i<N; i++){
-            arr[i] = Integer.parseInt(st.nextToken());
+            set.add(Integer.parseInt(st.nextToken()));
         }
-        Arrays.sort(arr);
         int M = Integer.parseInt(br.readLine());
         st = new StringTokenizer(br.readLine());
-        while(M > 0){
+        for(int i=0; i<M; i++){
             int x = Integer.parseInt(st.nextToken());
-            int start = 0;
-            int end = arr.length-1;
-            boolean ck = false;
-            while(start <= end){
-                int mid = (end + start)/2;
-                if(arr[mid] == x){
-                    sb.append(1).append('\n');
-                    ck = true;
-                    break;
-                }else if(arr[mid] < x){
-                    start = mid + 1;
-                }else{
-                    end = mid - 1;
-                }
-            }
-            if(!ck) sb.append(0).append('\n');
-            M--;
+            sb.append(set.contains(x)? "1" : "0").append("\n");
         }
         System.out.println(sb);
     }
