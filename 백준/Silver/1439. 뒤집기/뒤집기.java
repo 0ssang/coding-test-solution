0281233dd7d8015ac746b1008acd8a421d8d8f79
Youngsang Cho <51320595+0ssang@users.noreply.github.com>
2024-12-15 23:40:48 +0900

    [Silver V] Title: 뒤집기, Time: 104 ms, Memory: 14312 KB -BaekjoonHub

--- 백준/Silver/1439. 뒤집기/뒤집기.java
+++ 백준/Silver/1439. 뒤집기/뒤집기.java
@@ -1,12 +1,14 @@
+import java.io.*;
 import java.util.*;
+
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        StringBuilder sb = new StringBuilder();
-        String str = sc.nextLine();
-        for(int i = 0; i < str.length(); i++){
-            if(i == 0 || str.charAt(i) != str.charAt(i - 1)) sb.append(str.charAt(i));
-        }
-        System.out.println(sb.toString().length() /2);
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str = br.readLine();
+        StringTokenizer st_1 = new StringTokenizer(str, "0");
+        StringTokenizer st_2 = new StringTokenizer(str, "1");
+
+        System.out.println(Math.min(st_1.countTokens(), st_2.countTokens()));
     }
 }
