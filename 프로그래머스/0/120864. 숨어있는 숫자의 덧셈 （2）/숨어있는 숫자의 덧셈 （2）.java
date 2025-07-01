d2c34b5b4688fbfb6241905e17e23ad8a770558f
Youngsang Cho <51320595+0ssang@users.noreply.github.com>
2025-07-01 17:53:26 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.08 ms, Memory: 85.8 MB -BaekjoonHub

--- 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.java
+++ 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.java
@@ -1,19 +1,22 @@
 class Solution {
     public int solution(String mystr) {
-        String str = "";
         int sum = 0;
+        StringBuilder sb = new StringBuilder();
         
-        for(String s : mystr.split("")){
-            if(s.matches("[0-9]")){
-                str += s;
-            } else{
-                if(!str.isEmpty())
-                    sum += Integer.parseInt(str);
-                str = "";
+        for(char c : mystr.toCharArray()){
+            if(Character.isDigit(c)){
+                sb.append(c);
+            } else {
+                if(sb.length() > 0){
+                    sum += Integer.parseInt(sb.toString());
+                    sb.setLength(0);
+                }
             }
         }
-        if(!str.isEmpty())
-            sum += Integer.parseInt(str);
+        
+        if(sb.length() > 0){
+            sum += Integer.parseInt(sb.toString());
+        }
         
         return sum;
     }
