e38075e4ac372126645ddc87e5395ec02fd536bd
Youngsang Cho <51320595+0ssang@users.noreply.github.com>
2025-07-01 14:55:45 +0900

    [level 0] Title: 직각삼각형 출력하기, Time: 175.46 ms, Memory: 65.8 MB -BaekjoonHub

--- 프로그래머스/0/120823. 직각삼각형 출력하기/README.md
+++ 프로그래머스/0/120823. 직각삼각형 출력하기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 직각삼각형 출력하기 - 120823 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120823?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120823?language=java) 
 
 ### 성능 요약
 
-메모리: 32.2 MB, 시간: 103.31 ms
+메모리: 65.8 MB, 시간: 175.46 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 07월 29일 22:31:08
+2025년 07월 01일 14:55:43
 
 ### 문제 설명
 
--- 프로그래머스/0/120823. 직각삼각형 출력하기/직각삼각형 출력하기.java
+++ 프로그래머스/0/120823. 직각삼각형 출력하기/직각삼각형 출력하기.java
@@ -1,14 +1,17 @@
-import java.util.Scanner;
+import java.io.*;
 
 public class Solution {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j <= i; j++){
-                System.out.print("*");
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i<n; i++){
+            for(int j=0; j<=i; j++){
+                sb.append("*");
             }
-            System.out.println();
+            sb.append("\n");
         }
+        
+        System.out.print(sb);
     }
 }
