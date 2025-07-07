6c185d8d0c8af5ecae0fc457b1299f092c745295
Youngsang Cho <51320595+0ssang@users.noreply.github.com>
2025-07-07 19:33:46 +0900

    [level 1] Title: 폰켓몬, Time: 5.48 ms, Memory: 91.2 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/README.md
+++ 프로그래머스/1/1845. 폰켓몬/README.md
@@ -1,10 +1,10 @@
 # [level 1] 폰켓몬 - 1845 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1845?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1845?language=java) 
 
 ### 성능 요약
 
-메모리: 34.2 MB, 시간: 0.57 ms
+메모리: 91.2 MB, 시간: 5.48 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 03일 02:21:25
+2025년 07월 07일 19:33:44
 
 ### 문제 설명
 
--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -1,12 +1,12 @@
 import java.util.*;
+import java.util.stream.*;
+
 class Solution {
     public int solution(int[] nums) {
         int half = nums.length/2;
-        Set<Integer> set = new HashSet<>();
-        
-        for(int num : nums){
-            set.add(num);
-        }
+        Set<Integer> set = Arrays.stream(nums)
+            .boxed()
+            .collect(Collectors.toSet());
         
         return set.size() < half ? set.size() : half;
     }
