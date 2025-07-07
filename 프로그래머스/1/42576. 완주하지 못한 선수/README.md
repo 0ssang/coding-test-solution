2849157abb010f375561ac93464a6bd98debae8b
Youngsang Cho <51320595+0ssang@users.noreply.github.com>
2025-07-07 18:29:24 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 83.48 ms, Memory: 96.4 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/README.md
+++ 프로그래머스/1/42576. 완주하지 못한 선수/README.md
@@ -1,10 +1,10 @@
 # [level 1] 완주하지 못한 선수 - 42576 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576?language=java) 
 
 ### 성능 요약
 
-메모리: 67.5 MB, 시간: 35.13 ms
+메모리: 96.4 MB, 시간: 83.48 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 11일 23:37:07
+2025년 07월 07일 18:29:20
 
 ### 문제 설명
 
--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,21 +1,20 @@
 import java.util.*;
 class Solution {
     public String solution(String[] parti, String[] comp) {
-        Map<String, Integer> map = new HashMap<>();
-        // 해시맵에 참가자 명단 업데이트
+        Map<String,Integer> map = new HashMap<>();
+        
         for(String name : parti){
             map.put(name, map.getOrDefault(name, 0) + 1);
         }
-        // 완주자 명단에 들어 있으면 값 -1
+        
         for(String name : comp){
-            map.put(name, map.get(name) - 1);
+            if(map.get(name) > 1){
+                map.put(name, map.get(name) - 1);
+            } else {
+                map.remove(name);
+            }
         }
-        // 해시맵 검색하여 0보다 큰 값이 있으면 return
-        String res = "";
-        Set<String> keys = map.keySet();
-        for(String key : keys){
-            if(map.get(key) > 0)res = key;
-        }
-        return res;
+        
+        return map.keySet().iterator().next();
     }
 }
