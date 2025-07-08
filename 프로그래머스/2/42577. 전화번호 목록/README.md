af93d6a6a5128f55b8b0cc33c34e6f72165d87a7
Youngsang Cho <51320595+0ssang@users.noreply.github.com>
2025-07-08 23:38:14 +0900

    [level 2] Title: 전화번호 목록, Time: 336.68 ms, Memory: 99.7 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/README.md
+++ 프로그래머스/2/42577. 전화번호 목록/README.md
@@ -1,10 +1,10 @@
 # [level 2] 전화번호 목록 - 42577 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42577?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42577) 
 
 ### 성능 요약
 
-메모리: 101 MB, 시간: 236.22 ms
+메모리: 99.7 MB, 시간: 336.68 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 07일 18:35:27
+2025년 07월 08일 23:38:11
 
 ### 문제 설명
 
--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -2,20 +2,10 @@ import java.util.*;
 
 class Solution {
     public boolean solution(String[] pb) {
-        // 1. HashSet을 선언한다.
-        Set<String> set = new HashSet<>();
-
-        // 2. 모든 전화번호를 HashSet에 넣는다.
-        for(String s : pb){
-            set.add(s);
-        }
+        Arrays.sort(pb);
         
-        // 3. 모든 전화번호의 substring이 HashSet에 존재하는지 확인한다.
-        for(String s : pb){
-            for(int i = 0; i < s.length(); i++){
-                if(set.contains(s.substring(0, i)))
-                    return false;
-            }
+        for(int i=0; i<pb.length - 1; i++){
+            if(pb[i+1].startsWith(pb[i]) ) return false;
         }
 
         return true;
