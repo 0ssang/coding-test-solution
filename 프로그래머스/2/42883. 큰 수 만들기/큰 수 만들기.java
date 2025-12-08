871cb6a75cd82d7673a903e6b40980e21c86234e
Youngsang Cho <51320595+0ssang@users.noreply.github.com>
2025-12-08 19:10:43 +0900

    [level 2] Title: 큰 수 만들기, Time: 44.42 ms, Memory: 93.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String solution(String number, int k) {
+        StringBuilder sb = new StringBuilder();
+        
+        for(char c : number.toCharArray()){
+            while(sb.length() > 0 && k > 0 && sb.charAt(sb.length() - 1) < c){
+                sb.deleteCharAt(sb.length()-1);
+                k--;
+            }
+            sb.append(c);
+        }
+        
+        sb.setLength(sb.length() - k);
+        return sb.toString();
+    }
+}
