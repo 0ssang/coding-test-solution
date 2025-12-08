05d9363d8e350f59a4f0292bb4b16644a84074d5
Youngsang Cho <51320595+0ssang@users.noreply.github.com>
2025-12-08 19:10:56 +0900

    [level 2] Title: 구명보트, Time: 10.81 ms, Memory: 58 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] people, int limit) {
+        int left = 0, right = people.length-1, boats = 0;
+        Arrays.sort(people);
+        
+        while(left <= right){
+            if(people[left] + people[right] <= limit){
+                left++;
+            }
+            right--;
+            boats++;
+        }
+    
+        
+        return boats;
+    }
+}
